e0ff58f762e3503cfada8cb36499cc695c3bf18d
Dylan Heller <harrose@proton.me>
2026-07-20 10:44:33 +0000

    Exhaustive `Unsigned` and `Float` traits

--- src/traits.rs
+++ src/traits.rs
@@ -16,7 +16,68 @@ mod seal {
     impl SealedFloat for f64 {}
 }
 
-pub trait Unsigned: Clone + Copy + seal::SealedUnsigned {
+pub trait Unsigned:
+    Clone
+    + Copy
+    + Eq
+    + PartialEq
+    + Ord
+    + PartialOrd
+    + Hash
+    + Default
+    + Debug
+    + Display
+    + Binary
+    + Octal
+    + LowerHex
+    + UpperHex
+    + Add<Output = Self>
+    + AddAssign
+    + Sub<Output = Self>
+    + SubAssign
+    + Mul<Output = Self>
+    + MulAssign
+    + Div<Output = Self>
+    + DivAssign
+    + Rem<Output = Self>
+    + RemAssign
+    + Not<Output = Self>
+    + BitAnd<Output = Self>
+    + BitAndAssign
+    + BitOr<Output = Self>
+    + BitOrAssign
+    + BitXor<Output = Self>
+    + BitXorAssign
+    + Shl<u8, Output = Self>
+    + Shl<u16, Output = Self>
+    + Shl<u32, Output = Self>
+    + Shl<u64, Output = Self>
+    + Shl<u128, Output = Self>
+    + Shl<usize, Output = Self>
+    + ShlAssign<u8>
+    + ShlAssign<u16>
+    + ShlAssign<u32>
+    + ShlAssign<u64>
+    + ShlAssign<u128>
+    + ShlAssign<usize>
+    + Shr<u8, Output = Self>
+    + Shr<u16, Output = Self>
+    + Shr<u32, Output = Self>
+    + Shr<u64, Output = Self>
+    + Shr<u128, Output = Self>
+    + Shr<usize, Output = Self>
+    + ShrAssign<u8>
+    + ShrAssign<u16>
+    + ShrAssign<u32>
+    + ShrAssign<u64>
+    + ShrAssign<u128>
+    + ShrAssign<usize>
+    + Sum<Self>
+    + Product<Self>
+    + From<u8>
+    + FromStr
+    + seal::Sealed
+{
     fn lsb2_u8(self) -> u8;
     fn add_1_checked(self) -> Option<Self>;
     fn sub_1_checked(self) -> Option<Self>;
@@ -54,7 +115,34 @@ macro_rules! impl_trait_unsigned {
 impl_trait_unsigned!(u8, u16, u32, u64, u128, usize);
 
 pub trait Float:
-    Clone + Copy + From<f32> + Into<f64> + Div<Output = Self> + Mul<Output = Self> + seal::SealedFloat
+    Clone
+    + Copy
+    + Default
+    + PartialEq
+    + PartialOrd
+    + Debug
+    + Display
+    + LowerExp
+    + UpperExp
+    + UpperExp
+    + Neg<Output = Self>
+    + Add<Self, Output = Self>
+    + AddAssign<Self>
+    + Sub<Self, Output = Self>
+    + SubAssign<Self>
+    + Mul<Self, Output = Self>
+    + MulAssign<Self>
+    + Div<Self, Output = Self>
+    + Div<Self, Output = Self>
+    + DivAssign<Self>
+    + Rem<Self, Output = Self>
+    + RemAssign<Self>
+    + Sum<Self>
+    + Product<Self>
+    + From<f32>
+    + Into<f64>
+    + FromStr
+    + seal::Sealed
 {
     fn max_(self, b: Self) -> Self;
     fn is_finite_(self) -> bool;
